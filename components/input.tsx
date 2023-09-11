43d77919c7f170f33d3ca1d79c935292c972e312
Kaninika Sen <kaninikasc@gmail.com>
2023-09-11 19:23:43 +0530

    Second commit

--- components/input.tsx
+++ components/input.tsx
@@ -1,29 +1,23 @@
-import React from 'react';
+import React from "react";
 
 interface InputProps {
-    id: string;
-    onChange: any;
-    value:string;
-   label:string;
-   type?: string;
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
 }
 
-const Input: React.FC<InputProps> = ({
-    id, 
-    onChange,
-    value,
-    label,
-    type
- }) => {
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <div className="relative">
-    <input
-    onChange={onChange}
-    type={type}
-    value={value}
-    id = {id}
-      className="
-      block 
+      <input
+        onChange={onChange}
+        type={type}
+        value={value}
+        id={id}
+        className="
+    block 
       rounded-md
       px-6
       pt-6
@@ -37,10 +31,10 @@ const Input: React.FC<InputProps> = ({
       focus: ring-0
       peer
        "
-       placeholder=" "
+        placeholder=" "
       />
       <label
-      className="
+        className="
       absolute
       text-md
       text-zinc-400
@@ -52,15 +46,16 @@ const Input: React.FC<InputProps> = ({
       z-10
       origin-[0]
       left-6 
-      peer-placeholder- shown:scale-100
+      peer-placeholder-shown:scale-100
       peer-placeholder-shown:translate-y-0
       peer-focus:scale-75
       peer-focus:-translate-y-3"
-       htmlFor={id}>
-          {label} 
+        htmlFor={id}
+      >
+        {label}
       </label>
-      </div>
-  )
+    </div>
+  );
 };
 
 export default Input;
