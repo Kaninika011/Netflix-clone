9f1fac7b8b6676324a64d130eb052994e7b13738
Kaninika Sen <kaninikasc@gmail.com>
2023-11-29 09:44:31 +0530

    first commit

---
+++ pages/api/current.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method != )
+
+}
